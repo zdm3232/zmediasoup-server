b8720c88526597f42c856875b1b95bcae356e43c
zdm3232 <zim@EVOLV.localdomain>
2021-11-24 19:32:26 -0800

    config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,22 @@
 const os = require('os')
 const ifaces = os.networkInterfaces()
 
+const getLocalIp = () => {
+  let localIp = '127.0.0.1' // replace by public IP address
+  Object.keys(ifaces).forEach((ifname) => {
+    for (const iface of ifaces[ifname]) {
+      // Ignore IPv6 and 127.0.0.1
+      if (iface.family !== 'IPv4' || iface.internal !== false) {
+        continue
+      }
+      // Set the local ip to the first IPv4 address found and exit the loop
+      localIp = iface.address
+      return
+    }
+  })
+  return localIp
+}
+
 module.exports = {
   listenIp: '0.0.0.0',
   listenPort: 1,
@@ -54,7 +70,7 @@ module.exports = {
       listenIps: [
         {
           ip: '0.0.0.0',
-          announcedIp: '0.0.0.0' // replace by public IP address
+          announcedIp: getLocalIp()
         }
       ],
       maxIncomingBitrate: 1500000,
